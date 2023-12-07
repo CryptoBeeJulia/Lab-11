4e92445764ebba8a30af971c7a0156eb7890e6e4
Yuliia Bulakh <107890026+CryptoBeeJulia@users.noreply.github.com>
2023-12-07 22:25:05 +0200

    Update meal.js

--- meal.js
+++ meal.js
@@ -6,3 +6,40 @@
 
 //3- Link to search for meals using a keyword
 //https://www.themealdb.com/api/json/v1/1/search.php?s=
+
+
+const mealsElement = document.getElementById("meals");
+
+GetRandomMeal();
+
+async function GetRandomMeal()
+{
+    const resp = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    //console.log(resp);
+    const randomData = await resp.json();
+    //console.log(randomData);
+    const randomMeal = randomData.meals[0];
+    //console.log(randomMeal);
+
+    mealsElement.innerHTML = "";
+    addMeal(randomMeal);
+}
+
+function addMeal(mealData)
+{
+    const meal = document.createElement("div");
+    meal.classList.add("meal");
+
+    meal.innerHTML = ` <div class="meal-header">
+                            <span class="random">Meal of the Day</span>
+                            <img src="${mealData.strMealThumb}" alt="${mealData.strMeal}">
+                        </div>
+                        <div class="meal-body">
+                            <h3>${mealData.strMeal}</h3>
+                            <button class="fav-btn">
+                                <i class="fas fa-heart"></i>
+                            </button>
+                        </div>`
+
+    mealsElement.appendChild(meal);
+}
